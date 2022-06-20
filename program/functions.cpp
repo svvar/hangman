09922502fae97f9f9e1985bbaf478ac86dabe670
Oleksandr Shevchenko <aleksseva003@gmail.com>
2022-06-20 14:56:03 +0300

    deleted no used

--- program/functions.cpp
+++ program/functions.cpp
@@ -61,11 +61,6 @@ string charValidation(string ch, string &ch2, string word, bool &ind){ // фун
 		return "";
 	}
 
-	int wordSize = word.size()/2;
-	string * wordChars = new string[wordSize];
-	for(int i = 0, j = 0; i < wordSize; i++, j+=2)
-		wordChars[i] = word.substr(j, 2);
-
 	string lowAlphabet[] = {"а","б","в","г","ґ","д","е","є","ж","з","и","і","ї","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ь","ю","я"};
 	string upperAlphabet[] = {"А","Б","В","Г","Ґ","Д","Е","Є","З","Ж","И","І","Ї","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ь","Ю","Я"};	
 	
